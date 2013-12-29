1cbada35d43da6f5c950a8b8b410861e861b8a43
Jee Choi <jee@meow.(none)>
2013-12-28 20:33:25 -0500

    finished

--- cpu/amd/bobcat/e2-1800/main.cpp
+++ cpu/amd/bobcat/e2-1800/main.cpp
@@ -7,6 +7,8 @@
 #include <omp.h>
 #include <unistd.h>
 
+/* ======================================================== */
+/* Timer */
 namespace cpu {
 	
 	inline static uint64_t get_ticks_acquire() {
@@ -34,96 +36,98 @@ namespace cpu {
 	}
 
 }
+/* ======================================================== */
 
+/* Single and double precision sum squared functions */
 extern "C" void sumsq(const double* data, size_t length);
 extern "C" void sumsqf(const float* data, size_t length);
 
 int main(int argc, char** argv) {
+
+	/* Number of elements in the array */
 	const size_t array_length = 1024*1024*30;
-	
+
+	/* Create and initialize arrays */	
 	double* data0 = (double*)memalign(64, array_length * sizeof(double));
 	double* data1 = (double*)memalign(64, array_length * sizeof(double));
 	memset(data0, 0, array_length * sizeof(double));
 	memset(data1, 0, array_length * sizeof(double));
-	
+
+	/* Timers */	
 	double execTime0, execTime1;
+
+	/* Since the E2-1800 has 2 cores, we use OpenMP to run computation on both
+		 cores. 
+		 If the target processor has more cores, increase the number of data 
+		 structures, timers, OpenMP threads, etc. accordingly.
+	 */
 	#pragma omp parallel num_threads(3)
 	{
 		#pragma omp sections
 		{
+			/* OpenMP thread for first core */
 			#pragma omp section
 			{
-				fprintf (stderr, "Starting computation thread...\n");
-				sleep (2);
-
+				fprintf (stderr, "Starting first computation thread...\n");
 				#if(TYPE)
-				// double	
+				/* For double	precision */
 				{
+					fprintf (stderr, "Double precision...\n");
 					const uint64_t start0 = cpu::get_ticks_acquire();
 					sumsq(data0, array_length);
 					const uint64_t end0 = cpu::get_ticks_release();
-					// fprintf(stderr, "Ticks per DP element: %5.03lf\n", 
-									// double(end - start) / double(array_length));
+
 					execTime0 = double(end0 - start0)/1.7e+9;
 				}
 				#else
-				// single 
+				/* For single precision */
 				{
 					const uint64_t start0 = cpu::get_ticks_acquire();
 					sumsqf((const float*)data0, array_length * 2);
 					const uint64_t end0 = cpu::get_ticks_release();
-					// fprintf(stderr, "Ticks per SP element: %5.03lf\n", 
-									// double(end - start) / double(array_length * 2));
+
 					execTime0 = double(end0 - start0)/1.7e+9;
 				}
 				#endif
-
-				sleep (2);
 			}
+			/* OpenMP thread for second core */
 			#pragma omp section
 			{
-				fprintf (stderr, "Starting computation thread...\n");
-				sleep (2);
-
+				fprintf (stderr, "Starting second computation thread...\n");
 				#if(TYPE)
-				// double	
+				/* For double	precision */
 				{
 					const uint64_t start1 = cpu::get_ticks_acquire();
 					sumsq(data1, array_length);
 					const uint64_t end1 = cpu::get_ticks_release();
-					// fprintf(stderr, "Ticks per DP element: %5.03lf\n", 
-									// double(end - start) / double(array_length));
+
 					execTime1 = double(end1 - start1)/1.7e+9;
 				}
 				#else
-				// single 
+				/* For single precision */
 				{
 					const uint64_t start1 = cpu::get_ticks_acquire();
 					sumsqf((const float*)data1, array_length * 2);
 					const uint64_t end1 = cpu::get_ticks_release();
-					// fprintf(stderr, "Ticks per SP element: %5.03lf\n", 
-									// double(end - start) / double(array_length * 2));
+
 					execTime1 = double(end1 - start1)/1.7e+9;
 				}
 				#endif
 
 				sleep (2);
 			}
+			/* OpenMP thread for power measurement, if any */
 			#pragma omp section
 			{
-				fprintf (stderr, "Starting thread powermon...\n");
-				char powermon_cmd[1000]; 
-				#if(TYPE)
-					sprintf (powermon_cmd, "ssh -p 4004 jee@192.168.1.104  \"~/src/powermon/a.out /dev/ttyUSB0 32 1 15360 > ~/src/powermon/meow--bobcat--power--double--%d &\"", MAD_PER_ELEMENT);
-				#else
-					sprintf (powermon_cmd, "ssh -p 4004 jee@192.168.1.104  \"~/src/powermon/a.out /dev/ttyUSB0 32 1 15360 > ~/src/powermon/meow--bobcat--power--single--%d &\"", MAD_PER_ELEMENT);
-				#endif
-				fprintf (stderr, "%s\n", powermon_cmd);
-				int re = system (powermon_cmd);
+				fprintf (stderr, "Starting powermon thread...\n");
+				/* Power measurement code start here */
+			
+				/* Power measurement code end here */
 			}
 		}
 	}
 
+	/* Number of flops and bytes executed */
 	#if(TYPE)	
 		double flops = 2 * array_length * MAD_PER_ELEMENT * 3.0;
 	#else
@@ -131,8 +135,11 @@ int main(int argc, char** argv) {
 	#endif
 	double bytes = 2 * array_length * sizeof(double);
 
+	/* take the maximum of the execution times of the two cores */
 	double execTime;
 	if(execTime0 > execTime1) execTime = execTime0; else execTime = execTime1;
+
+	/* Print performance info */
 	fprintf (stderr, "Execution time 0: %lf\tExecution time 1: %lf\n", 
 					execTime0, execTime1);
 	fprintf (stderr, "Execution time: %lf\n", execTime);
@@ -141,7 +148,7 @@ int main(int argc, char** argv) {
 	fprintf (stderr, "Bandwidth: %lf GB/s\n", bytes/execTime/1.0e+9);
 	fprintf (stderr, "Performance: %lf GFLOPS\n", flops/execTime/1.0e+9);
 
-	
+	/* Free memory */	
 	free(data0);	
 	free(data1);	
 }
